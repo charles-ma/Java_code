4c95cfe6d953f4c3893dc93177685435bdec51bd
Chao Ma <machao@seas.upenn.edu>
2013-01-19 09:49:03 -0500

    frame for method timings

---
+++ MethodTimings/timings/Timings.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package timings;
+
+/**
+ * @author machao
+ *
+ */
+public class Timings {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
