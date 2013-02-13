7f05af021bf88dd637cd4c92fabf4d79b63976d6
Chao Ma <machao@seas.upenn.edu>
2013-02-13 10:00:52 -0500

    finish functions other than next() in tokenizer

--- Tokenizer&Parser/tokenizer/BnfTokenizer.java
+++ Tokenizer&Parser/tokenizer/BnfTokenizer.java
@@ -3,8 +3,10 @@
  */
 package tokenizer;
 
+import java.io.IOException;
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 /**
  * This class can tokenize the input stream
@@ -17,29 +19,37 @@ public class BnfTokenizer implements Iterator<Token> {
 	private Token preToken = null;
 	private boolean isBack = false;
 	private boolean isVirgin = true;
+	private LinkedList<Integer> peekedNotRead = new LinkedList<Integer> ();
 	
 	/**
 	 * Constructor of the BnfTokenizer
 	 * @param reader the stream to be tokenized
 	 */
 	public BnfTokenizer(Reader reader) {
+		if(reader == null) throw new IllegalArgumentException();
 		this.reader = reader;
 	}
 		
 	@Override
 	public boolean hasNext() {
 		if(this.isBack) return true;
-		return false;
+		try {
+			if(peekChar() == -1) return false;
+		} catch (IOException e) {
+			System.err.println("Something wrong while reading!");
+			return false;
+		}
+		return true;
 	}
 
 	@Override
 	public Token next() {
 		if(!hasNext()) throw new UnsupportedOperationException("Don't have any tokens left!");
-		this.isVirgin = false;
 		if(this.isBack) {
 			this.isBack = false;
 			return this.preToken;
 		}
+		this.isVirgin = false;
 		return null;
 	}
 
@@ -56,4 +66,30 @@ public class BnfTokenizer implements Iterator<Token> {
 		this.isBack = true;
 	}
 
+	/**
+	 * Peeks the next character in the input stream
+	 * @return ascii value of the character (-1 if reaches end of the stream)
+	 * @throws IOException
+	 */
+	public int peekChar() throws IOException{
+		if(this.peekedNotRead.size() != 0) return this.peekedNotRead.peek();
+		int asciiPeeked = this.reader.read();
+		this.peekedNotRead.add(asciiPeeked);
+		return asciiPeeked;
+	}
+	
+	/**
+	 * Gets the next character in the input stream
+	 * @return the character
+	 * @throws IOException
+	 */
+	public char getChar() throws IOException{
+		if(peekChar() == -1) throw new IOException("No more characters!");
+		if(this.peekedNotRead.size() != 0) {
+			int ascii = this.peekedNotRead.remove();
+			return (char)ascii;
+		} else {
+			return (char)this.reader.read();
+		}
+	}
 }
