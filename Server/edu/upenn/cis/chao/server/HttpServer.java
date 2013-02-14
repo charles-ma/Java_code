22d636c041b69605559221e0fb7db807afca3d25
Charles Ma <mcrichard.buaa@gmail.com>
2013-02-14 16:11:36 -0500

    initial work for the http server

---
+++ Server/edu/upenn/cis/chao/server/HttpServer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.upenn.cis.chao.server;
+
+/**
+ * @author charles
+ * @version 1.0
+ */
+public class HttpServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Server/edu/upenn/cis/chao/server/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains a http server
+ */
+/**
+ * @author charles
+ * @version 1.0
+ */
+package edu.upenn.cis.chao.server;
