8df429bdd5b94cf50db7afb3e7a63d2bee94b81c
Chao Ma <machao@seas.upenn.edu>
2013-02-21 22:18:35 -0500

    bnf class

---
+++ BNF/bnf/BNF.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package bnf;
+
+/**
+ * @author machao
+ *
+ */
+public class BNF {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
