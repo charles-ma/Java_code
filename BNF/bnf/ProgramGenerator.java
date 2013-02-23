0310c61aae15367878753ef2e11247a10469a2cf
Chao Ma <machao@seas.upenn.edu>
2013-02-23 13:06:49 -0500

    finish bnf project

--- BNF/bnf/ProgramGenerator.java
+++ BNF/bnf/ProgramGenerator.java
@@ -50,7 +50,12 @@ public class ProgramGenerator {
 		}
 		bnf.read(reader);
 		bnf.write(writer);
-		System.out.print(writer.toString());
+		System.out.print("The Grammar is:\n" + writer.toString() + "----------------------\n");
+		for(int i = 1; i <= 3; i++) {
+			System.out.println("Sample Program " + i + " :");
+			printList(expand("program"));
+			System.out.println("----------------------");
+		}
 	}
 	
 	/**
@@ -68,14 +73,62 @@ public class ProgramGenerator {
 	 * @return the expanding result
 	 */
 	public static List<String> expand(String term) {
+		if(term == null) return null;
+		BNF.Tree<Token> rule = bnf.lookUp(term);
+		if(rule != null) return expand(rule);
 		return null;
 	}
 
+	/**
+	 * Given a token, expands it into a List of terminals
+	 * @param rule the tree of which the value is the token to be expanded
+	 * @return the expanding result
+	 */
+	public static List<String> expand(BNF.Tree<Token> rule) {
+		ArrayList<String> result = new ArrayList<String>();
+		if(rule == null) return null;
+		Token value = rule.getValue();
+		Random rand = new Random();
+		ArrayList<Tree<Token>> subtrees = rule.getChildren();
+		for(int i = subtrees.size() - 1; i >= 0; i--) {
+			Tree<Token> subtree = subtrees.get(i);
+			if(value.getType() == Token.TokenType.SEQUENCE) {
+				result.addAll(expand(subtree));
+			} else if(value.getType() == Token.TokenType.ANYNUM) {
+				int num = rand.nextInt(10);
+				for(int j = 0; j < num; j++) {
+					result.addAll(expand(subtree));
+				}
+			} else if(value.getType() == Token.TokenType.OPTION) {
+				int num = rand.nextInt(2);
+				for(int j = 0; j < num; j++) {
+					result.addAll(expand(subtree));
+				}
+			} else if(value.getType() == Token.TokenType.OR) {
+				int num = subtrees.size();
+				num = rand.nextInt(num);
+				result.addAll(expand(subtrees.get(num)));
+				break;
+			} else {
+				
+			}
+		}
+		if(value.getType() == Token.TokenType.NONTERMINAL) {
+			result.addAll(expand(value.getValue()));
+		} else if(value.getType() == Token.TokenType.TERMINAL) {
+			result.add(value.getValue());
+		}
+		return result;
+	}
+	
 	/**
 	 * Print out the strings (which should all be terminals), separated by spaces
 	 * @param list the list to be printed out
 	 */
 	public static void printList(List<String> list) {
-		
+		for(int i = 0; i < list.size(); i++) {
+			if(!list.get(i).equals(";")) System.out.print(list.get(i) + " ");
+			else System.out.print(list.get(i) + "\n");
+		}
 	}
 }
--- BNF/bnf/ProgramGeneratorTest.java
+++ BNF/bnf/ProgramGeneratorTest.java
@@ -2,23 +2,23 @@ package bnf;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
 public class ProgramGeneratorTest {
 
 	@Test
-	public void testMain() {
-		
-	}
-
-	@Test
 	public void testExpand() {
-		
+		ProgramGenerator p = new ProgramGenerator("bnf/Grammar.txt");
+		List<String> result = p.expand("fileName");
+		assertEquals(1, result.size());
+		String s = result.get(0);
+		assertTrue(s.equals("a") || s.equals("b") || s.equals("c") || s.equals("d") || s.equals("e"));
+		result = p.expand("Num");
+		assertEquals(1, result.size());
+		s = result.get(0);
+		assertTrue(s.equals("0") || s.equals("1")); 
 	}
-
-	@Test
-	public void testPrintList() {
-		
-	}
-
+	
 }
